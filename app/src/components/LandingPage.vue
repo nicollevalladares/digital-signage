46a006400f85b3be468fe5918a4ea5a8b1fc2000
Nicolle Valladares <nicollevalladares@gmail.com>
2019-06-27 14:08:26 -0600

    prueba axios

--- app/src/components/LandingPage.vue
+++ app/src/components/LandingPage.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
     <h2>UUID: {{uuid}}</h2>
+    <h2>prueba: {{prueba}}</h2>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'LandingPage',
   data(){
     return {
       uuid: process.env.RESIN_DEVICE_UUID || 'No UUID - Development Machine',
+      prueba: null
     }
+  },
+  methods:{
+    
+  },
+  created(){
+    axios.get('http://connect.dev.hn/playlists')
+    .then(response => {
+      this.prueba = response;
+    })
   }
 }
 </script>
