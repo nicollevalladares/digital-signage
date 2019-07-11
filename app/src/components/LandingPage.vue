3698cc14214762ffc8e5b55166aaa05cc9b2ab75
benavidez25 <dilverbenavides@hotmail.com>
2019-07-11 16:03:08 -0600

    docker-compose

--- app/src/components/LandingPage.vue
+++ app/src/components/LandingPage.vue
@@ -1,34 +1,20 @@
 <template>
   <div>
-    <h2>UUID: {{uuid}}</h2>
-    <h2>prueba: {{prueba}}</h2>
-    <h2>project: {{project}}</h2>
-    <h2>appid: {{appid}}</h2>
-    
-
+      <img src="@/assets/loading.svg">
   </div>
 </template>
+
+
 
 <script>
 import axios from 'axios'
 var download = require('download-file')
-// function formatBytes(a,b){if(0==a)return"0 Bytes";var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
 
 export default {
   name: 'LandingPage',
   data(){
     return {
-      prueba: null,
-      project: process.env.npm_package_productName || 'Only Available in Development Mode',
-      projectver: process.env.npm_package_version || 'Only Available in Development Mode',
-      projectdesc: process.env.npm_package_description || 'Only Available in Development Mode',
-      projectmaintainer: process.env.npm_package_author_name || 'Only Available in Development Mode',
-      uuid: process.env.RESIN_DEVICE_UUID || 'No UUID - Development Machine',
-      appid: process.env.RESIN_APP_ID || 'No APPID - Development Machine',
-      appname: process.env.RESIN_APP_NAME || 'No Data - Development Machine',
-      devtype: process.env.RESIN_DEVICE_TYPE || 'No Data - Development Machine',
-      osver: process.env.RESIN_HOST_OS_VERSION || 'No Data - Development Machine',
-      node: process.versions.node,
+       prueba : null
     }
   },
   methods:{
@@ -37,7 +23,7 @@ export default {
   created(){
     axios.get('http://connect.dev.hn/playlists')
     .then(response => {
-      this.prueba = response + process.env.BALENA_DEVICE_UUID;
+      this.prueba = response;
     })
 
   // var options = {
@@ -54,6 +40,13 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style >
+ body {
+   background-color: rgb(240, 245, 224)
+ }
 
+ img {
+   margin-top: auto;
+   margin-bottom: auto;
+ }
 </style>
