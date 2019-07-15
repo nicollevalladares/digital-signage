b4457fda45d1cc2bf6cdb07f13351db83b24fb87
nicollevalladares <nicollevalladares@gmail.com>
2019-07-15 16:00:28 -0600

    add screen con sockets

--- vue/app/src/components/AddScreen.vue
+++ vue/app/src/components/AddScreen.vue
@@ -1,16 +1,32 @@
 <template>
     <div id="newScreen">
-        <button>
-            <router-link :to="{name: 'Player'}">Player</router-link>
-        </button>
-        <h1 id="newScreenTitle">CONFIGURACIÓN DE NUEVA PANTALLA</h1>
-        <hr>
-        <h3 id="newScreenDescription">Esta pantalla no está vinculada a ninguna cuenta en Digital Signage.</h3>
-        <h3>Diríjase a <span id="url">www.digitalsignage.com/configuration</span> e ingrese el siguiente código para realizar la configuración</h3>
-        <div id="idScreen">9437618452</div>
-        <h3 id="qr-text">o escanee el siguiente código QR:</h3>
-        <br>
-        <img id="barCode" :src="src">
+        <div v-if="!scanned">
+             <h1 id="newScreenTitle">CONFIGURACIÓN DE NUEVA PANTALLA</h1>
+            <hr>
+            <h3 id="newScreenDescription">Esta pantalla no está vinculada a ninguna cuenta en Digital Signage.</h3>
+            <h3>Diríjase a <span id="url">www.digitalsignage.com/configuration</span> e ingrese el siguiente código para realizar la configuración</h3>
+            <div id="idScreen">9437618452</div>
+            <h3 id="qr-text">o escanee el siguiente código QR:</h3>
+            <br>
+            <img id="barCode" :src="src">
+        </div>
+
+        <div v-if="scanned && !configured">
+            <img style="width: 250px; height:250px;" src="../assets/loading.svg">
+			<!-- <progress value="6" max="10"></progress> -->
+			<div style="color: white;" id="output">
+                <h1>Esperando Configuración</h1>
+            </div>
+        </div>
+
+        <div v-if="configured">
+			<div style="color: white;" id="output">
+                <h1>Configuración Finalizada</h1>
+                <hr>
+                <h2>Descargando Archivos...</h2>
+            </div>
+        </div>
+       
     </div>
 </template>
 
@@ -18,6 +34,7 @@
 
 <script>
 import axios from 'axios'
+import { mapActions, mapState } from 'vuex'
 
 export default {
     name: 'AddScreen',
@@ -26,13 +43,40 @@ export default {
     },
     data(){
         return {
-            src: null
+            src: null,
+            idScreen: '9437618452',
+            scanned: false,
+            configured: false
+        }
+    },
+    sockets: {
+        connect: function () {
+            // console.log('socket connected')
+        },
+        customEmit: function (data) {
+            // console.log('this method was fired by the socket server. eg: io.emit("customEmit", data)')
         }
     },
     methods: {
+        ...mapActions(['getScreenInfo'])
     },
     created(){
-        this.src = "https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=http://signage.dev.hn/configQR/9437618452";
+        this.getScreenInfo({idScreen: this.idScreen}),
+        this.sockets.subscribe(this.idScreen, (data) => {
+            console.log(data.type);
+            if (data.type=='scanned'){
+                this.scanned = true
+            }
+
+            else if(data.type=='configured'){
+                this.configured = true
+            }
+            // store.dispatch('updateScreens',{ data })
+        }),
+        this.src = "https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=http://signage.dev.hn/configQR/9437618452"
+    },
+    computed: {
+      ...mapState(['screen'])
     }
 }
 </script>
--- vue/app/src/store.js
+++ vue/app/src/store.js
@@ -21,13 +21,42 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    uuid: process.env.RESIN_DEVICE_UUID
+    uuid: process.env.RESIN_DEVICE_UUID,
+    screen: ''
   },
   mutations: {
-    
+    setScreenInfo(state, screenInfo){
+      state.screen = screenInfo;
+    },
+    // updateScreen(state, data){
+    //   if(state.screen.id == data.idScreen){
+    //       // doc.active = data.screen.active
+    //     }
+    // }
   },
   actions: {
-    
+    getScreenInfo({commit}, payload){
+      const id = payload.idScreen;
+      const screenInfo = [];
+
+      axios.get("http://connect.dev.hn/screens")
+      .then(response => {
+          response.data.forEach(doc => {
+            if(doc.idScreen == id){
+              let screen = doc;
+              screenInfo.push(screen);
+            }
+
+            // console.log(screenInfo);
+            
+          });
+      })
+      commit('setScreenInfo', screenInfo);
+    },
+    // updateScreens({commit}, payload){
+    //   const data = payload.data
+    //   commit('updateScreen', data)
+    // },
   },
   getters: {
 
