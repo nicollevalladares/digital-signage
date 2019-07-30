20d21d8b3627ebeeea7d13a00f251faf6c4e527f
benavidez25 <dilverbenavides@hotmail.com>
2019-07-30 10:43:33 -0600

    screenshot

--- vue/app/screenshot.js
+++ vue/app/screenshot.js
@@ -1,31 +1,24 @@
 
 var express = require("express")
-// var cors = require('cors')
 const app = express()
 const path = require('path')
 const fs = require('fs')
 const screenshot = require('screenshot-desktop')
-// app.use(cors())
-
 app.use(express.static(path.join(__dirname, 'screenshot')))
-// app.use('screenshot/',express.static(__dirname+"screenshot/"));
 
-//create folder to save media files
 fs.exists('screenshot',function(exists){
   if(!exists)
       fs.mkdirSync('screenshot');
 }) 
 
 app.get('/', function (req, res) {
-  // res.send(process.env.RESIN_DEVICE_UUID || 'No UUID-Development machine')
-  var name = process.env.RESIN_DEVICE_UUID +'_'+ Date.now();
   screenshot({ filename: 'screenshot/actuallyScreen.png' }).then((imgPath) => {
     res.sendFile(path.join(__dirname, './screenshot', 'actuallyScreen.png'))
   })
 })
 
-app.listen(80, function () {
-  // console.log('Screenshot ready');
+app.listen('80', function () {
+   console.log('Screenshot ready');
 })
 
 
