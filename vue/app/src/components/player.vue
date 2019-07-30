a4843599352dd7f7aeec10f355c597acf151cb6d
benavidez25 <dilverbenavides@hotmail.com>
2019-07-30 16:24:18 -0600

    Screenshot

--- vue/app/src/components/player.vue
+++ vue/app/src/components/player.vue
@@ -301,9 +301,6 @@ export default {
                if(data.data.appSelected != 'DownloadFiles')
                   this.sockets.unsubscribe(this.key.key.toString());
             }
-            if (data.type == 'screenshot'){
-                axios.get(`${servers.FTPServer}/screenshot`)
-            }
       })
   }
 }
