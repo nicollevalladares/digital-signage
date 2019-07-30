36e58a9854eaa76559f90b1f3153d3c39743ecd1
benavidez25 <dilverbenavides@hotmail.com>
2019-07-30 11:05:56 -0600

    screenshot

--- vue/app/screenshot.js
+++ vue/app/screenshot.js
@@ -2,14 +2,23 @@
 var express = require("express")
 const app = express()
 const path = require('path')
+const http = require('http')
 const fs = require('fs')
 const screenshot = require('screenshot-desktop')
-app.use(express.static(path.join(__dirname, 'screenshot')))
+var server = http.createServer(app);
+var bodyParser = require("body-parser")
+var cors = require('cors')
 
 fs.exists('screenshot',function(exists){
   if(!exists)
       fs.mkdirSync('screenshot');
 }) 
+
+app.use(cors())
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+
+app.use(express.static(path.join(__dirname, 'screenshot')))
 
 app.get('/', function (req, res) {
   screenshot({ filename: 'screenshot/actuallyScreen.png' }).then((imgPath) => {
@@ -17,7 +26,7 @@ app.get('/', function (req, res) {
   })
 })
 
-app.listen('80', function () {
+server.listen(80, function () {
    console.log('Screenshot ready');
 })
 
